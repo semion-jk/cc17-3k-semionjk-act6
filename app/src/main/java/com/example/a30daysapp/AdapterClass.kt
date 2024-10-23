cf612ccabf708b67d66bbc85c07993449683e715
naysu06 <franzdominicc@gmail.com>
2024-10-23 15:47:47 +0800

    update

--- app/src/main/java/com/example/a30daysapp/AdapterClass.kt
+++ app/src/main/java/com/example/a30daysapp/AdapterClass.kt
@@ -5,13 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterClass(private val dataList: ArrayList<DataClass>): RecyclerView.Adapter<AdapterClass.ViewHolderClass>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.cardDesign, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
         return ViewHolderClass(itemView)
     }
 
--- app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dataList: ArrayList<DataClass>
     lateinit var imageList:Array<Int>
     lateinit var dateList: Array<String>
-    lateinit var titleList: Array<String>
+    lateinit var titleList: MutableList<String>
     lateinit var textList: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +23,46 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        titleList = mutableListOf<String>()
+
+        for (i in 1..30){
+            val dayString = "Day $i"
+            titleList.add(dayString)
+        }
+
+        dateList = arrayOf(
+            "Start Your Day with a Morning Routine",
+            "Drink More Water",
+            "10-Minute Meditation",
+            "Set a Goal for Today",
+            "Go for a Walk"
+
+        )
+
         imageList = arrayOf(
-            R.drawable.day1)
+            R.drawable.day1,
+            R.drawable.day2,
+            R.drawable.day3,
+            R.drawable.day4,
+            R.drawable.day5
+        )
+
+        textList = arrayOf(
+            "Establishing a morning routine boosts productivity, reduces stress, and sets a positive tone for the rest of the day",
+            "Staying hydrated improves focus, increases energy levels, and supports overall bodily functions, including digestion and circulation",
+            "Meditation reduces anxiety, enhances emotional health, and improves attention span, helping you stay calm and focused throughout the day",
+            "Setting a daily goal provides direction and motivation, leading to a sense of accomplishment as you check off tasks",
+            "Walking improves physical health, boosts mood, and enhances creativity by giving your brain a break from everyday stressors"
+
+        )
+
+        recyclerView =  findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        dataList = arrayListOf<DataClass>()
+        getData()
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
